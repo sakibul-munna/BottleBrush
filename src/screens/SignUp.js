eeee88aac8122c0633bdedd53d335dfaed01623b
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-02-13 17:17:17 +0600

    Update SignUp.js

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -22,8 +22,6 @@ const SignUp = (props) => {
             <View style={styles.viewStyle}>
                 <Card>
                     <Image style={styles.tinyLogo} source={require('./../../assets/logo.png')} />
-                     //<Card.Title style={styles.textStyle}>Welcome to BottleBrush</Card.Title>
-                    //<Card.Divider />
                     <Input
                         leftIcon={<MaterialIcons name="person" size={20} color="#de3358" />}
                         placeholder='Name'
